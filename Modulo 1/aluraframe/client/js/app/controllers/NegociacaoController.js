8d3b84888dd4ccf11df16a40548ed0541d0e28a6
SoulReaperNZ <veloso.deivid@gmail.com>
2016-10-15 18:44:26 -0300

    Corrigindo Date formato

--- Modulo 1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Modulo 1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,62 @@
 class NegociacaoController {
-    adiciona(event){
-        event.preventDefault();
+
+        constructor(){
         
         let $ = document.querySelector.bind(document);
         
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+            
+        };
 
+    adiciona(event){
+        event.preventDefault();
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        //Esta dando erro por que meu model recebe data 
+        //e a controller esta passando string, não reconhece o método getTime()
+        //console.log(typeof(this._inputData.value));
+        // let negociacao = new Negociacao(
+        //     this._inputData.value,
+        //     this._inputQuantidade.value,
+        //     this._inputValor.value
+        // );
+
+        //Para concerter uma data elegantemente, 
+        //o Date funcina fazendo um join de elementos por virgula(2016,03,30)
+        //Para fazer isso usar o split ou replace
+        //console.log(new Date(this._inputData.value.split('-')));
+        //dessa forma ele converte corretamente;
+        //console.log(trataData(this._inputData.value));
+
+        let novaData = trataData(this._inputData.value);
+        console.log(new Date(novaData));
+        //console.log(negociacao);
+
+        //Para passar os parametros no construtor 
+        //do método dinamicamento com ... cada ponto representa um parametro que será criado no split
+          console.log(new Date(...
+                this._inputData.value
+                .split('-')
+                //percorre o array e devolve o item, e pega a posição (indice) que estou passando
+                .map(function(item,indice){
+                    return item - indice % 2
+                })
+            ));
     };
+
+   
     
+};
+
+function trataData(data){
+
+        var dataAlt = [data.split('-')];
+        if(dataAlt[1] == 1){
+            dataAlt[1] = 0;
+        }else{
+            dataAlt[1] = dataAlt[1] - 1;
+        };
+            data = dataAlt;
+    return dataAlt;
 };
