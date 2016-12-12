2353b1d6cca1243a11bb3a908e0fd8a0d2af0303
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-12-12 14:03:38 -0200

    Add Proxy

--- Modulo 1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Modulo 1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,26 +6,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        // this._listaNegociacoes = new ListaNegociacoes(function(model){
-        //Essa função é chamada no adiciona e esvazia metodos da classe ListaNegociacao
-        //Nesse escopo da função esse this que retorna é do método que executou a função,
-        //ou seja é o this.armadilha(this) que no caso vem do contexto da ListaNegociacao
-        //Por isso o erro Cannot read property 'update' of undefined(…)
-        //Porque a ListaNegociacao não tem o a instancia do metodo update e sim a NegociacaoController
-        //     this._negociacoesView.update(model);
-        // });
-
-        //Para revolver esse problema, vamos usar o this dinamico a nosso favor
-        //Primeiro vamos passar  o this como parametro, esse this é o da minha classe NegociacaoController
-        //Na ListaNegociacoes constructor vou receber esse this como contexto
-        // this._listaNegociacoes = new ListaNegociacoes(function (this, model) {
-        //     this._negociacoesView.update(model);
-        // });
-
-        //RESOLVENDO SEM O CONTEXTO E COM ARROW function
-        //Arrow function não é só um modo de encurtar a forma de escrita 
-        //Ela não tem scopo dinamico ela é lexico o escopo não muda
-        //Por isso o this dessa função é do NegociacaoController e não de quem chama.
+       
         this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'))
@@ -76,47 +57,3 @@ class NegociacaoController {
         this._inputData.focus(); // Para dar o foco no campo
     }
 };
-
-// function trataData(data){
-
-//         var dataAlt = [data.split('-')];
-//         if(dataAlt[1] == 1){
-//             dataAlt[1] = 0;
-//         }else{
-//             dataAlt[1] = dataAlt[1] - 1;
-//         };
-//             data = dataAlt;
-//     return dataAlt;
-// };
-
-//Esta dando erro por que meu model recebe data 
-        //e a controller esta passando string, não reconhece o método getTime()
-        //console.log(typeof(this._inputData.value));
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     this._inputQuantidade.value,
-        //     this._inputValor.value
-        // );
-
-        //Para concerter uma data elegantemente, 
-        //o Date funcina fazendo um join de elementos por virgula(2016,03,30)
-        //Para fazer isso usar o split ou replace
-        //console.log(new Date(this._inputData.value.split('-')));
-        //dessa forma ele converte corretamente;
-        //console.log(trataData(this._inputData.value));
-
-       // let novaData = trataData(this._inputData.value);
-        //console.log(new Date(novaData));
-        //console.log(negociacao);
-
-        //Para passar os parametros no construtor 
-        //do método dinamicamento com ... cada ponto representa um parametro que será criado no split
-        //  let data = new Date(...
-        //         this._inputData.value
-        //         .split('-')
-        //         //percorre o array e devolve o item, e pega a posição (indice) que estou passando
-        //         .map((item,indice) => item - indice % 2)
-        //     );
-
-    //let diaMesAno = negociacao.data.getDate() + "/" + (negociacao.data.getMonth() + 1) + "/" + negociacao.data.getFullYear();
-    //console.log(diaMesAno);
--- Modulo 1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ Modulo 1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,26 +1,12 @@
 class ListaNegociacoes {
 
     //Estou recebendo meu this da classe NegociacaoController como um contexto
-    constructor(armadilha) {
+    constructor() {
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
 
     adiciona(negociacao) {
-
         this._negociacoes.push(negociacao);
-        //Depois que add na lista, executa a função dentrro da variavel armadilah que vem do NegociacaoController
-        //Executo passando o this como parametro que siginifica minha lista de negociação.
-        //this._armadilha(this);
-
-        //Para resolver o problema de contexto do this dinamico vou usar a classe Reflect.apply do java script
-        //Ela recebe primeiro o método qu eu quero chamar
-        //Segundo quem é o contexto que eu desejo executar essa função (qual é o this) 
-        //Qual é o parametro que essa função precisa receber? é a lista de negociações no caso é o [this]
-        //[this] Representa a minha model no new ListaNegociacoes(function (this, model) 
-        //Reflect.apply(this._armadilha, this._contexto, [this]);
-
-        this._armadilha(this);
     }
 
     get negociacoes() {
@@ -28,8 +14,44 @@ class ListaNegociacoes {
     }
 
     esvazia() {
-        this._armadilha(this);
         this._negociacoes = [];
         //Reflect.apply(this._armadilha, this._contexto, [this]);
     }
 }
+
+
+// class ListaNegociacoes {
+
+//     //Estou recebendo meu this da classe NegociacaoController como um contexto
+//     constructor(armadilha) {
+//         this._negociacoes = [];
+//         this._armadilha = armadilha;
+//     }
+
+//     adiciona(negociacao) {
+
+//         this._negociacoes.push(negociacao);
+//         //Depois que add na lista, executa a função dentrro da variavel armadilah que vem do NegociacaoController
+//         //Executo passando o this como parametro que siginifica minha lista de negociação.
+//         //this._armadilha(this);
+
+//         //Para resolver o problema de contexto do this dinamico vou usar a classe Reflect.apply do java script
+//         //Ela recebe primeiro o método qu eu quero chamar
+//         //Segundo quem é o contexto que eu desejo executar essa função (qual é o this) 
+//         //Qual é o parametro que essa função precisa receber? é a lista de negociações no caso é o [this]
+//         //[this] Representa a minha model no new ListaNegociacoes(function (this, model) 
+//         //Reflect.apply(this._armadilha, this._contexto, [this]);
+
+//         this._armadilha(this);
+//     }
+
+//     get negociacoes() {
+//         return [].concat(this._negociacoes);
+//     }
+
+//     esvazia() {
+//         this._armadilha(this);
+//         this._negociacoes = [];
+//         //Reflect.apply(this._armadilha, this._contexto, [this]);
+//     }
+// }
