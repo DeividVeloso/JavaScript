fe2d9a0a79c153c5e2f375e34a0ea48a51cd002c
DeividVeloso <veloso.deivid@gmail.com>
2017-06-28 09:45:35 -0300

    Module Pattern

--- Modulo 1/aluraframe/client/js/app/services/ConnectionFactory.js
+++ Modulo 1/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,40 +1,45 @@
-var dbName = "aluraframe";
-var dbVersion = 4;
-var stores = ["negociacoes"];
-class ConnectionFactory {
-  //para evitar que o programador tente instanciar esssa classe, pois ela é static
-  constructor() {
-    throw new Error("Não é possível criar instâncias de ConnectionFactory");
-  }
 
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      //Cria o banco de dados
-      let openRequest = window.indexedDB.open(dbName, dbVersion);
-
-      openRequest.onupgradeneeded = e => {
-        this._createObjectStore(e.target.result);
-      };
-
-      openRequest.onsuccess = e => {
-          resolve(e.target.result)
-      };
-
-      openRequest.onerror = e => {
-          reject(e.target.error.name)
-      };
-    });
-  }
-
-  static _createObjectStore(connection) {
-    //Verifica se já existe as stores que estou passando no array STORES para criar no banco
-    //Caso exista então apagar as existentes, senão criar as novas stores
-    stores.forEach(store => {
-      if (connection.objectStoreNames.contains(store)) {
-        connection.deleteObjectStore(store);
-      } else {
-        connection.createObjectStore(store, { autoIncrement: true });
+var ConnectionFactory = (function (){
+    var dbName = "aluraframe";
+    var dbVersion = 4;
+    var stores = ["negociacoes"];
+    var connection = null;
+    return class ConnectionFactory {
+      //para evitar que o programador tente instanciar esssa classe, pois ela é static
+      constructor() {
+        throw new Error("Não é possível criar instâncias de ConnectionFactory");
       }
-    });
-  }
-}
+
+      static getConnection() {
+        return new Promise((resolve, reject) => {
+          //Cria o banco de dados
+          let openRequest = window.indexedDB.open(dbName, dbVersion);
+
+          openRequest.onupgradeneeded = e => {
+            this._createObjectStore(e.target.result);
+          };
+
+          openRequest.onsuccess = e => {
+            if(!connection) connection = e.target.result;
+              resolve(connection)
+          };
+
+          openRequest.onerror = e => {
+              reject(e.target.error.name)
+          };
+        });
+      }
+
+      static _createObjectStore(connection) {
+        //Verifica se já existe as stores que estou passando no array STORES para criar no banco
+        //Caso exista então apagar as existentes, senão criar as novas stores
+        stores.forEach(store => {
+          if (connection.objectStoreNames.contains(store)) {
+            connection.deleteObjectStore(store);
+          } else {
+            connection.createObjectStore(store, { autoIncrement: true });
+          }
+        });
+      }
+    }
+})()
